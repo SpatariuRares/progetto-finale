723314c9dc3e654367c3ada2949d573b30317191
spata00 <sararares12@gmail.com>
2021-11-22 10:43:44 +0100

    rares

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,6 +83,7 @@
                 <div class="title m-b-md">
                     Laravel Lorenzo
                     Laravel Simone
+                    rares spatariu hambareanu constantin
                 </div>
 
                 <div class="links">
